a485fc337ce53bd4f51bbe4f5f445845d4c9d94e
Ziegler Michael (extern) <Michael.Ziegler@funkwerk-itk.com>
2020-10-13 19:51:57 +0200

    Add my ship model

--- src/client/SpaaaceRenderer.js
+++ src/client/SpaaaceRenderer.js
@@ -13,6 +13,7 @@ export default class SpaaaceRenderer extends Renderer {
         return {
             ship1: 'assets/ship1.png',
             ship2: 'assets/ship2.png',
+            ship4: 'assets/airplane.png',
             missile: 'assets/shot.png',
             bg1: 'assets/space3.png',
             bg2: 'assets/space2.png',
