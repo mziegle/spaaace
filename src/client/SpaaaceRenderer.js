5c8a483403871f24fbf3b23fa49922a8b72ae900
Ziegler Michael (extern) <Michael.Ziegler@funkwerk-itk.com>
2020-10-15 19:18:42 +0200

    Next ship

--- src/client/SpaaaceRenderer.js
+++ src/client/SpaaaceRenderer.js
@@ -14,6 +14,7 @@ export default class SpaaaceRenderer extends Renderer {
             ship1: 'assets/ship1.png',
             ship2: 'assets/ship2.png',
             ship4: 'assets/airplane.png',
+            ship42: 'assets/ship42.png',
             missile: 'assets/shot.png',
             bg1: 'assets/space3.png',
             bg2: 'assets/space2.png',
